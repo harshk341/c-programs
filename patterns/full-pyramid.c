d4b4fd2b946ddff0ff09e0fb4e3e1b6dc53ebe4d
Harsh Kumar <harsh341kumar@gmail.com>
2024-07-07 18:11:25 +0530

    full-pyramid pattern

---
+++ patterns/full-pyramid.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int rows;
+
+    printf("Enter a number of rows:\n");
+    scanf("%d", &rows);
+
+    for (int i = 0; i < rows; i++)
+    {
+
+        for (int j = 0; j < 2 * (rows - i) - 1; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < 2 * i + 1; k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
