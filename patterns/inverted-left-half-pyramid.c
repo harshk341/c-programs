0389b07615be039276ea359a96effed5b0c27050
Harsh Kumar <harsh341kumar@gmail.com>
2024-07-08 22:52:53 +0530

    inverted-left-half-pyramid pattern

---
+++ patterns/inverted-left-half-pyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+    printf("Enter the number of rows:\n");
+    scanf("%d", &rows);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("  ");
+        }
+
+        for (int k = 0; k < rows - i; k++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
